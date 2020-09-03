ffb23253f16d5709871814ba245ad9b82d79143a
20170612 <695527585@qq.com>
2020-09-03 16:07:46 +0800

    更新更新跨域配置

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -40,8 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'bindings',
-            \Fruitcake\Cors\HandleCors::class,
+            'bindings'
         ],
     ];
 
